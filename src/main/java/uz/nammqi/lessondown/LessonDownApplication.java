4d8ae37f5726bd409aa8b9533fc0b157919dddd9
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-03-11 09:30:56 +0500

    Commit
    Commit

--- src/main/java/uz/nammqi/lessondown/LessonDownApplication.java
+++ src/main/java/uz/nammqi/lessondown/LessonDownApplication.java
@@ -5,10 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uz.nammqi.lessondown.models.Department;
 import uz.nammqi.lessondown.models.Faculties;
 import uz.nammqi.lessondown.service.DepartmentService;
@@ -30,11 +26,7 @@ public class LessonDownApplication implements CommandLineRunner {
 
     }
 
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("uz.nammqi.lessondown")).build();
-    }
+
 
     @Override
     public void run(String... args) throws Exception {
